08d1c8deb58fe2dd16ace878970c0636cf18ce20
Sraleik <nicolas.rotier@gmail.com>
2017-03-20 23:29:52 +0100

    put the theme in place

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,14 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
 var connect = require('gulp-connect-php');
 
 
-gulp.task('sass', function () {
-  return gulp.src('assets/sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('assets/css'))
-    .pipe(browserSync.reload({
-      stream: true
-    }));
-});
-
-gulp.task('watch',['browserSync', 'sass'], function(){
-  gulp.watch('./assets/sass/**/*.scss', ['sass']);
+gulp.task('watch',['browserSync'], function(){
+  gulp.watch('./assets/css/*.css').on('change', browserSync.reload);
   gulp.watch('*.php').on('change', browserSync.reload);
 });
 
 gulp.task('browserSync', function(){
-  // browserSync({
-  //   server: {
-  //     baseDir: 'index.php',
-  //     proxy: 'localhost:3000'
-  //   },
-  // })
   connect.server({}, function (){
     browserSync({
       proxy: 'localhost',
